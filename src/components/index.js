0c030e00fdf3955aef5b1417cc62c9f6e0a4c467
AshokkSS <ashokks@chillibean.com>
2023-06-13 16:08:37 +0100

    Emptied repo

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import Header from "./presentational/Header";
-import NoteForm from "./container/NoteForm";
-import Note from "./presentational/Note";
-import NotesContainer from "./container/NotesContainer";
-export { Header, NoteForm, Note, NotesContainer };
